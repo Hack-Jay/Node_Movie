b5a0eb5db7bcee1d8fd87c0613fea005b5eadf43
hack_jay <735059024@qq.com>
2017-11-11 18:00:02 +0800

    the less

--- config/routes.js
+++ config/routes.js
@@ -2,45 +2,45 @@
  * Created by Administrator on 2017/6/26 0026.
  */
 
-var Index=require('../app/controllers/index');
-var User=require('../app/controllers/user');
+var Index = require('../app/controllers/index');
+var User = require('../app/controllers/user');
 //var Post=require('../app/controllers/post')
-var Movie=require('../app/controllers/movie');
-var Comment=require('../app/controllers/comment')
-var Category=require('../app/controllers/category')
+var Movie = require('../app/controllers/movie');
+var Comment = require('../app/controllers/comment')
+var Category = require('../app/controllers/category')
 
-module.exports=function(app) {
-//pre handle user
-app.use(function(req,res,next){
-        app.locals.user=req.session.user;   //app.locals为本地的常量，可挂载信息
+module.exports = function(app) {
+    //pre handle user
+    app.use(function(req, res, next) {
+        app.locals.user = req.session.user; //app.locals为本地的常量，可挂载信息
         next();
-})
+    })
 
-    app.get('/',Index.index);
-    app.get('/movie-index',Index.indexs)
-//user
-    app.post('/user/signup',User.signup);       //提交注册页面
-    app.post('/user/signin',User.signin);       //提交登录页面
-    app.get('/sign-denglu',User.showSignin);
-    app.get('/sign-zhuce',User.showSignup);
-    app.get('/logout',User.logout);
+    app.get('/', Index.index);
+    app.get('/movie-index', Index.indexs)
+        //user
+    app.post('/user/signup', User.signup); //提交注册页面
+    app.post('/user/signin', User.signin); //提交登录页面
+    app.get('/sign-denglu', User.showSignin);
+    app.get('/sign-zhuce', User.showSignup);
+    app.get('/logout', User.logout);
     app.get('/admin/user/list', User.signinRequired, User.adminRequired, User.list);
     // //post
     // app.get('/admin/post/new',Post.new);
     // app.post('/admin/post/new',Post.new);
 
-//movie
+    //movie
 
-    app.get('/admin/movie/new',User.signinRequired,User.adminRequired,Movie.new);
-    app.post('/admin/movie/new',User.signinRequired,User.adminRequired,Movie.save);
-    app.get('/movie/:id',User.signinRequired,User.adminRequired,Movie.detail);
-    app.get('/admin/movie/list',User.signinRequired,User.adminRequired,Movie.list)
-    app.get('/admin/update/:id',User.signinRequired,User.adminRequired,Movie.update)
-    //app.delete('/admin/movie/list',movie.delete)
-    app.delete('/admin/movie/list',User.signinRequired,User.adminRequired,Movie.deleted );
+    app.get('/admin/movie/new', User.signinRequired, User.adminRequired, Movie.new);
+    app.post('/admin/movie/new', User.signinRequired, User.adminRequired, Movie.save);
+    app.get('/movie/:id', User.signinRequired, User.adminRequired, Movie.detail);
+    app.get('/admin/movie/list', User.signinRequired, User.adminRequired, Movie.list)
+    app.get('/admin/update/:id', User.signinRequired, User.adminRequired, Movie.update)
+        //app.delete('/admin/movie/list',movie.delete)
+    app.delete('/admin/movie/list', User.signinRequired, User.adminRequired, Movie.deleted);
 
     //comment
-    app.post('/admin/comment',User.signinRequired,User.adminRequired,Comment.save )
+    app.post('/admin/comment', User.signinRequired, User.adminRequired, Comment.save)
 
     //category
     app.get('/admin/category/new', User.signinRequired, User.adminRequired, Category.new);
@@ -49,12 +49,12 @@ app.use(function(req,res,next){
     app.get('/admin/category/list', User.signinRequired, User.adminRequired, Category.list);
 
     //404
-    app.get('/404', function (req, res) {
-        res.render('404',{
-            message:'',
-            url:'/'
-        });
-    })
-//
+    app.get('/404', function(req, res) {
+            res.render('404', {
+                message: '',
+                url: '/'
+            });
+        })
+        //
 
 }
--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-var express=require('express');
-var path=require('path');
+var express = require('express');
+var path = require('path');
 
 var bodyParser = require('body-parser');
-var session=require('express-session');
-var cookieParser=require('cookie-parser');
-var mongoose=require('mongoose');
-var mongoStore=require('connect-mongo')(session);
-var port=process.env.PORT||3000;
-var ejs=require('ejs');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var mongoose = require('mongoose');
+var mongoStore = require('connect-mongo')(session);
+var port = process.env.PORT || 3000;
+var ejs = require('ejs');
 
-var app=express();
-//var dbUrl='mongodb://localhost/blogtest';
-var dbUrl='mongodb://root:123456@ds151973.mlab.com:51973/node_test'
+var app = express();
+//var dbUrl = 'mongodb://localhost/blogtest';
+var dbUrl = 'mongodb://root:123456@ds151973.mlab.com:51973/node_test'
 
 mongoose.connect(dbUrl);
 console.log('Connect Suceess!');
-app.set('/views',path.join(__dirname,'./views'));
-app.engine('html',ejs.__express);
+app.set('/views', path.join(__dirname, './views'));
+app.engine('html', ejs.__express);
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
 //表单解析
@@ -24,10 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(session({
-    secret:'blog',
-    store:new mongoStore({
-        url:dbUrl,
-        collection:'session'
+    secret: 'blog',
+    store: new mongoStore({
+        url: dbUrl,
+        collection: 'session'
     })
 }));
 
@@ -38,4 +38,4 @@ require('./config/routes')(app);
 
 app.listen(port);
 
-console.log('start on port'+port);
+console.log('start on port' + port);
